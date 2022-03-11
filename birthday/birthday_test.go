816b7efdec81efc785d6f6fcff3a4af4ae7e7370
Eduardo <eduardo.jimpen@gmail.com>
2022-03-11 22:41:21 +0000

    Test for PeopleWhoseBirthdayIsToday. Month and day is defined automaticly

--- birthday/birthday_test.go
+++ birthday/birthday_test.go
@@ -1,8 +1,54 @@
 package birthday
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+)
 
 func TestPeopleWhoseBirthdayIsToday(t *testing.T) {
+
+	date := time.Now().Format(layoutUK)
+	today := strings.Split(date, "/")
+	monthToday := today[1]
+	dayToday := today[2]
+
+	var birthdaysSample = &birthday{
+		peopleList: people{
+			person{
+				"Today", "Always", "2001/" + monthToday + "/" + dayToday,
+			},
+			person{
+				"Today 2", "Always", "2018/" + monthToday + "/" + dayToday,
+			},
+			person{
+				"Lee", "David", "1990/05/2",
+			},
+			person{
+				"Lopez", "Jennifer", "1970/10/29",
+			},
+			person{
+				"Muller", "David", "1995/5/2",
+			},
+		},
+	}
+
+	birthdaysExpected := &people{
+		person{
+			"Today", "Always", "2001/" + monthToday + "/" + dayToday,
+		},
+		person{
+			"Today 2", "Always", "2018/" + monthToday + "/" + dayToday,
+		},
+	}
+
+	peopleChecked := birthdaysSample.PeopleWhoseBirthdayIsToday()
+
+	if !reflect.DeepEqual(peopleChecked, birthdaysExpected) {
+		t.Errorf("%+v did not equal to %+v", peopleChecked, birthdaysExpected)
+	}
+
 }
 
 func TestPeopleWhoseBirthdayIs(t *testing.T) {

ad69ce536f9303c41d70547316e6033f62769a41
Eduardo <eduardo.jimpen@gmail.com>
2022-03-11 22:37:52 +0000

    Defining the test functions

---
+++ birthday/birthday_test.go
@@ -0,0 +1,15 @@
+package birthday
+
+import "testing"
+
+func TestPeopleWhoseBirthdayIsToday(t *testing.T) {
+}
+
+func TestPeopleWhoseBirthdayIs(t *testing.T) {
+}
+
+func TestIsLeapYear(t *testing.T) {
+}
+
+func TestJsonToPeople(t *testing.T) {
+}

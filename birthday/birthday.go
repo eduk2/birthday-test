f215e8c076c034592f8aecf2c3ded7daf1e1b2c6
Eduardo <eduardo.jimpen@gmail.com>
2022-03-11 22:42:15 +0000

    Definited PeopleWhoseBirthdayIsToday and added a const to parse dates

--- birthday/birthday.go
+++ birthday/birthday.go
@@ -1,13 +1,18 @@
 package birthday
 
-type person struct {
-	LastName string `json:"lastName"`
-	Name     string `json:"firstName"`
-	Birthday string `json:"Birthday"`
-}
-type people []person
+// Parse dates from json (year/month/day)
+const layoutUK string = "2006/1/2"
 
-type birthday struct {
+type person struct {
+	LastName   string `json:"lastName"`
+	Name       string `json:"firstName"`
 	jsonFile   string
 	peopleList people
+}
+
+// Given a JSON list of people with their dates of birth,
+// returns people whose birthday is today.
+func (b *birthday) PeopleWhoseBirthdayIsToday() (peopleBirthdayTodayList *people) {
+
+	return
 }
